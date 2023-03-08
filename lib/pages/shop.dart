2279e27fc50286b51f7b77a1054a8732b61ee451
ashen8981 <ashenmalindu112@gmail.com>
2023-03-08 11:29:37 +0530

    Search bar is done........

--- lib/pages/shop.dart
+++ lib/pages/shop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_ui/util/shop_grid.dart';
 
 class UserShop extends StatelessWidget {
   const UserShop({Key? key}) : super(key: key);
@@ -6,7 +7,53 @@ class UserShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child:Text("shop")),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Shop',
+              style: TextStyle(color: Colors.black),
+            ),
+            Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(24.0),
+                  child: Icon(Icons.shopping_bag_outlined,color: Colors.black),
+                ),
+                Icon(Icons.menu,color: Colors.black) ],
+            ),
+          ],
+        ),
+
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                color: Colors.grey[300],
+                child: Row(
+                  children: [
+                    Icon(Icons.search,color: Colors.grey[500],),
+                    Container(
+                      child: Text('Search',style: TextStyle(color: Colors.grey[500]),),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 2),
+          const Expanded(
+            child: ShopGrid(),
+          ),
+        ],
+      )
+
     );
   }
 }
