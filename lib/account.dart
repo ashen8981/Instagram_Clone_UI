be2da9dba1ece322780f9752cbc85ffbf60f96e5
ashen8981 <ashenmalindu112@gmail.com>
2023-03-08 11:29:37 +0530

    account header is done......

--- lib/account.dart
+++ lib/account.dart
@@ -6,7 +6,52 @@ class UserAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child:Text("acc")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0,top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    shape: BoxShape.circle
+                  ),
+                ),
+
+                //number of followers,posts,following
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                    Column(
+                      children:const  [
+                        Text('250',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
+                        Text('Posts'),
+                      ],
+                    ),
+                    Column(
+                      children:const  [
+                        Text('150',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
+                        Text('Following'),
+                      ],
+                    ),
+                    Column(
+                      children:const  [
+                        Text('143',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
+                        Text('Followers'),
+                      ],
+                    ),
+                  ],),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
