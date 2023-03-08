fbf759cf5f41f0f7cdf734ce13c6059e5360175d
ashen8981 <ashenmalindu112@gmail.com>
2023-03-08 11:29:37 +0530

    Search bar is done........

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_ui/util/explore_grid.dart';
 
 class UserSearch extends StatelessWidget {
   const UserSearch({Key? key}) : super(key: key);
@@ -6,7 +7,25 @@ class UserSearch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child:Text("search")),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Container(
+            color: Colors.grey[300],
+            child: Row(
+              children: [
+                Icon(Icons.search,color: Colors.grey[500],),
+                Container(
+                  child: Text('Search',style: TextStyle(color: Colors.grey[500]),),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: ExploreGrid(),
     );
   }
 }
